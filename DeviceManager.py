0f9a08aef110e3256ffe211920c18446df2c7d9e
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-09-28 09:24:26 +0200

    .

--- DeviceManager.py
+++ DeviceManager.py
@@ -153,32 +153,3 @@ def change_switch_case():
         print(create_vlan())
     elif choice == 'b':
         print(setup_snmp())
-
-
-class WindowControl(tk.Tk):
-
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
-        container = tk.Frame(self)
-
-        container.pack(side="top", fill="both", expand=True)
-
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
-
-        self.frames = {}
-
-        for F in (StartPage, PageOne, PageTwo):
-            frame = F(container, self)
-
-            self.frames[F] = frame
-
-            frame.grid(row=0, column=0, sticky="nsew")
-
-        self.show_frame(StartPage)
-
-    def show_frame(self, cont):
-        frame = self.frames[cont]
-        frame.tkraise()
-
-
